4866f869b44f0a3b5cfec1a57f6145dc95fa8a98
Adam Bortkun <a.bortkun@jmblab.com>
2021-07-16 16:47:51 +0200

    Dodawanie liczb z tablicy za pomocą foreach

--- Kurs z CSharp/Kurs z CSharp/Program.cs
+++ Kurs z CSharp/Kurs z CSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Threading.Channels;
 
 namespace Kurs_z_CSharp
@@ -7,16 +8,16 @@ namespace Kurs_z_CSharp
     {
         static void Main(string[] args)
         {
-            double x = 34.1;
-            double a = x + 2.1;
-            double b = x - 2.1;
-            double c = x * 2.1;
-            double d = x / 2.1;
-            Console.WriteLine("x = 34.1");
-            Console.WriteLine("wynik dodania 2.1 do x: " + a);
-            Console.WriteLine("wynik odjęcia 2.1 od x: " + b);
-            Console.WriteLine("wynik przemnozenia 2.1 przez x: " + c);
-            Console.WriteLine("wynik podzielenia x przez 2.1: " + d);
+            var numbers = new[] {3.5, 4.3, 8.5, 9.5};
+            double result = 0;
+
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+
+            Console.WriteLine(result);
+
 
         }
     }
